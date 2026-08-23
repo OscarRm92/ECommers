601c7f7a078a819dd733c084d6adb880ed30e92f
Oscar Ramirez <oscar.ramirez92@outlook.com>
2026-08-23 15:12:26 -0600

    mostrar en product detail + closed

--- react-ecommerce/src/Components/ProductDetail/index.jsx
+++ react-ecommerce/src/Components/ProductDetail/index.jsx
@@ -1,15 +1,36 @@
+import { useContext } from 'react'
 import { XMarkIcon} from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
+    const context = useContext(ShoppingCartContext)
+    // console.log('Product show', context.productToShow);
+    
+    
     return(
-        <aside className='product-detail flex flex-col fixed right-0 border border-black rounded bg-white '>
+        <aside 
+        className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white `}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl' >Datail</h2>
                 <dix>
-                    <XMarkIcon className='h-6 w-6 text-black'></XMarkIcon>
+                    <XMarkIcon 
+                        className='h-6 w-6 text-black cursor-pointer'
+                        onClick={() => context.clouseProductDetail()} 
+                    ></XMarkIcon>
                 </dix>
             </div>
+            <figure className='px-6'>
+                    <img 
+                        className='w-full h-full rounded-lg'
+                        src={context.productToShow.images[0]} 
+                        alt={context.productToShow.title}></img>
+                </figure>
+                <p className='flex flex-col p-6'>
+                    <span className='font-medium text-2xl mb-2'>${context.productToShow.price}</span>
+                    <span className='font-medium text-md' >{context.productToShow.title}</span>
+                    <span className='font-light text-sm'>{context.productToShow.description}</span>
+                </p>
         </aside>
     )
 }
--- react-ecommerce/src/Context/index.jsx
+++ react-ecommerce/src/Context/index.jsx
@@ -3,14 +3,34 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartContextProvider = ({children}) => {
+    /**
+     * Shopint card · Increment quantity
+     */
     const [count, setCount] = useState(0)
+
     // console.log('count', count);
-    
-    
+    /**
+     * Product Detal · Open/Clouse 
+     */
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const clouseProductDetail = () => setIsProductDetailOpen(false)
+
+    /**
+     * Product Detail · Show product
+     */
+    const [productToShow, setProductToShow] = useState({})
+
+
     return(
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            clouseProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
