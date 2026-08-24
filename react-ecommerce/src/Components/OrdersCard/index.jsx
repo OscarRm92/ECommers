66403fe0820bf8f16296732da9390c219e195b5b
Oscar Ramirez <oscar.ramirez92@outlook.com>
2026-08-23 21:59:09 -0600

    estilizado orders y order

--- react-ecommerce/src/Components/OrdersCard/index.jsx
+++ react-ecommerce/src/Components/OrdersCard/index.jsx
@@ -1,15 +1,26 @@
-import { XMarkIcon , CurrencyDollarIcon } from '@heroicons/react/24/outline'
+import { ChevronRightIcon , CurrencyDollarIcon } from '@heroicons/react/24/outline'
 
 const OrdersCard = props  =>{
     const {totalProducts, totalPrice,date} = props
 
     return(
-        <div className='flex justify-between items-center rounded-lg bg-white mb-3 border border-black'>
-           <p>
+        <div className='flex justify-between items-center rounded-lg bg-white border border-black w-90 p-4 mb-3'>
+           <div className='flex justify-between w-full'>
+            <p className='flex flex-col'>
                 <span>{date}</span>
-                <span>{totalPrice}</span>
-                <span>{totalProducts}</span>
-           </p>
+                <span className='font-light'>{totalProducts} articles</span>
+            </p>
+            <p className='flex justify-center items-center gap-2'>
+                <div className='flex items-center'>
+                     <CurrencyDollarIcon className='h-6 w-6  text-black cursor-pointer '></CurrencyDollarIcon>
+                    <span className='font-medium text-2xl'>{totalPrice}</span>
+                </div>
+                <div>
+                    <ChevronRightIcon className='h-6 w-6  text-black cursor-pointer '></ChevronRightIcon>
+                </div>
+
+            </p>
+           </div>
         </div>
     )
 }
